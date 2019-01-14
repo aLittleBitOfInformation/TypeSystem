94e8ef735a4d2bb4c94e572c31dce46069ea1c54
Rezana <ganierezana8@gmail.com>
2019-01-14 13:58:09 +0200

    feat: added project

---
+++ TypeSystem/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace TypeSystem
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Type stringType = typeof(string);
+            Console.WriteLine(stringType.Attributes);
+
+            string currency = $"{2.5:C}";
+            Console.Write("Currency:    $\"{2.5:C}\"    ");
+            Console.WriteLine(currency);
+
+            decimal value = 2.5M;
+            string fixedPoint = $"{value:F2}";
+            Console.Write("Fixed Point: $\"{2.5:F2}\"   ");
+            Console.WriteLine(fixedPoint);
+
+            decimal percentageValue = 0.25M;
+            string percentage = $"{percentageValue:P}";
+            Console.Write("Percentage:  $\"{0.25:P}\"   ");
+            Console.WriteLine(percentage);
+        }
+    }
+}
